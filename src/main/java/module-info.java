7001fc18fcc80932166dc1d2a10548afb242d78a
phamngocdo <ngocdo992k4@gmail.com>
2024-10-03 09:06:25 +0700

    Initialize

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module app {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
     opens app.run to javafx.fxml;
     opens app.controller to javafx.fxml;
