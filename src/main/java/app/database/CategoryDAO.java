589692754abcccddc8a5a2c4b4d5cdf0c69bc765
phamngocdo <ngocdo992k4@gmail.com>
2024-10-13 20:52:31 +0700

    Update

--- src/main/java/app/database/CategoryDAO.java
+++ src/main/java/app/database/CategoryDAO.java
@@ -2,20 +2,67 @@ package app.database;
 
 import app.base.Category;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.sql.*;
 
 public class CategoryDAO {
-    public static final String MAIN_TABLE = "categories";
-
+    // Lấy ra các thể loại khi biết docId.
     public static ArrayList<Category> getAllCategoryFromDocId(String docId) {
-        return new ArrayList<>();
+        ArrayList<Category> categories = new ArrayList<>();
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT c.category_id, c.category FROM categories AS c ");
+        query.append("JOIN document_category AS dc ON c.category_id = dc.category_id ");
+        query.append("WHERE dc.document_id = ?");
 
+        try {
+            PreparedStatement preparedStatement;
+            preparedStatement = DatabaseManagement.getConnection().prepareStatement(query.toString());
+            preparedStatement.setString(1, docId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                categories.add(new Category(
+                        resultSet.getString("category_id"),
+                        resultSet.getString("category")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return categories;
     }
 
+    // Thêm thể loại mới.
     public static void addCategory(Category category) {
-        category.setId(DatabaseManagement.createRandomIdInTable(MAIN_TABLE,
-                "category_id"));
-        //
+        StringBuilder query = new StringBuilder();
+        query.append("INSERT INTO categories ");
+        query.append("(category_id, category) ");
+        query.append("VALUES (?, ?)");
+        try {
+            PreparedStatement preparedStatement;
+            preparedStatement = DatabaseManagement.getConnection().prepareStatement(query.toString());
+            preparedStatement.setString(1, category.getCategory());
+            preparedStatement.setString(2, category.getCategory());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean checkCategoryExist(String category) {
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT category_id FROM categories ");
+        query.append("WHERE category = ?");
+
+        try {
+            PreparedStatement preparedStatement;
+            preparedStatement = DatabaseManagement.getConnection().prepareStatement(query.toString());
+            preparedStatement.setString(1, category);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next(); // Nếu có kết quả thì thể loại tồn tại
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
