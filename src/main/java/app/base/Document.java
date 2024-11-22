70f3bee4e5b3ad81cdca7811a236e848de703e79
phamngocdo <ngocdo992k4@gmail.com>
2024-11-22 17:36:49 +0700

    Update

--- src/main/java/app/base/Document.java
+++ src/main/java/app/base/Document.java
@@ -48,26 +48,6 @@ public class Document {
         this.ratings = Objects.requireNonNullElseGet(ratings, () -> RatingDAO.getAllRatingFromDocId(id));
     }
 
-    public Document(ResultSet resultSet) throws SQLException {
-        if (resultSet != null) {
-            id = resultSet.getString("document_id");
-            title = resultSet.getString("title");
-            isbn = resultSet.getString("isbn");
-            quantity = resultSet.getInt("quantity");
-            remaining = resultSet.getInt("remaining");
-            ratingCount = resultSet.getInt("ratings_count");
-            averageScore = resultSet.getDouble("average_score");
-            imageUrl = resultSet.getString("image_url");
-            pageCount = resultSet.getInt("page_count");
-            description = resultSet.getString("description");
-            publisher = resultSet.getString("publisher");
-            publishedDate = resultSet.getString("published_date");
-            authors = AuthorDAO.getAllAuthorFromDocId(id);
-            categories = CategoryDAO.getAllCategoryFromDocId(id);
-            ratings = RatingDAO.getAllRatingFromDocId(id);
-        }
-    }
-
     public Document(String id) {
         Document doc = DocumentDAO.getDocFromId(id);
         if (doc != null) {
@@ -83,6 +63,26 @@ public class Document {
             description = doc.getDescription();
             publisher = doc.getPublisher();
             publishedDate = doc.getPublishedDate();
+            authors = AuthorDAO.getAllAuthorFromDocId(id);
+            categories = CategoryDAO.getAllCategoryFromDocId(id);
+            ratings = RatingDAO.getAllRatingFromDocId(id);
+        }
+    }
+
+    public Document(ResultSet resultSet) throws SQLException {
+        if (resultSet != null) {
+            id = resultSet.getString("document_id");
+            title = resultSet.getString("title");
+            isbn = resultSet.getString("isbn");
+            quantity = resultSet.getInt("quantity");
+            remaining = resultSet.getInt("remaining");
+            ratingCount = resultSet.getInt("ratings_count");
+            averageScore = resultSet.getDouble("average_score");
+            imageUrl = resultSet.getString("image_url");
+            pageCount = resultSet.getInt("page_count");
+            description = resultSet.getString("description");
+            publisher = resultSet.getString("publisher");
+            publishedDate = resultSet.getString("published_date");
             authors = AuthorDAO.getAllAuthorFromDocId(id);
             categories = CategoryDAO.getAllCategoryFromDocId(id);
             ratings = RatingDAO.getAllRatingFromDocId(id);
